e15ad1730a5e744d599f50d7030b80ed9ca72a63
Wen <qinwen.shi@gmail.com>
2014-07-05 20:57:38 +0800

    try selenium webdriver

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
+require 'selenium/webdriver'
 
 require 'capybara/cucumber'
 
-Capybara.default_driver = :selenium
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
